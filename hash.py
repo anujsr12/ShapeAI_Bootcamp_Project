cdb7566c7f06e7c1b24b0f0dd2dc276dc3739b44
Anuj Srivastava <50262640+anujsr12@users.noreply.github.com>
2021-07-05 17:52:56 +0530

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,9 +1,15 @@
 #!usr/bin/env python
 
 import hashlib
-
+#taking input as a string
 st = raw_input("Enter string : ")
+
+#converting the string into md5
 md5st = hashlib.md5(st.encode())
-sha1st = hashlib.sha1(str(md5st).encode())
-sha256st = hashlib.sha256(str(sha1st).encode())
+
+#now coverting the md5 data to sha1 by taking the md5st data as string and then coverting it to sha1
+sha1st = hashlib.sha1(str(md5st.hexdigest()).encode())
+
+#now coverting the md5 data to sha1 by taking the sha1st data as string and then coverting it to sha256
+sha256st = hashlib.sha256(str(sha1st.hexdigest()).encode())
 print sha256st.hexdigest()
